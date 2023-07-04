ff02e58f4ae117c0abf0582b18cfecfc7cbcd403
Vivek Singh <viveksinghup.singh@gmail.com>
2023-07-05 01:10:56 +0530

    clock app

---
+++ clockApp/script.js
@@ -0,0 +1,28 @@
+const hour_el = document.getElementById('hr');
+const minute_el = document.getElementById('mn');
+const second_el = document.getElementById('sec');
+const ampm_el = document.getElementById('pm');
+
+
+function updateClock() {
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    var ampm = 'AM';
+
+    if(hour > 12){
+        hour = hour-12;
+        ampm = 'PM';
+    }
+
+    hour_el.innerText = hour;
+    minute_el.innerText = minute;
+    second_el.innerText = second;
+    ampm_el.innerText = ampm;
+
+    setTimeout(function(){
+        updateClock();
+    }, 1000)
+}
+updateClock();
